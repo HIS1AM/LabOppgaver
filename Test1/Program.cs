ecbad1eb9755184aae1a2365734bc8f22d723264
pshis <pshis@HISHAM>
2022-09-01 11:14:55 +0200

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int alder = 8;
+                
+
+            if (alder > 17)
+            {
+                //Console.WriteLine("Gammel nok til å kjøre bil");
+            }
+            else
+            {
+                //Console.WriteLine("ikke Gammel nok til å kjøre bil");
+            }
+
+
+            
+            
+
+
+            ///her er en oppg 0109
+            ///oppg om temp
+            ///
+            Console.WriteLine("tast inn temp");
+            //lese inn verdi fra console
+            string stringTemp=Console.ReadLine();
+            //konvertere stringen til et tall ved å bruke parse
+            int temp=int.Parse(stringTemp);
+
+            if(temp>0)
+            {
+                Console.WriteLine("Plussgrader");
+            }
+            else if(temp<0)
+            {
+                Console.WriteLine("minusgrader");
+            }
+            else
+            {
+                Console.WriteLine("det er null");
+            }
+
+            //if (temp == 0)
+            //{
+            //    Console.WriteLine("det er 0");
+            //}
+
+
+            //en ny oppg
+
+            Console.ReadKey();
+
+        }
+    }
+}
